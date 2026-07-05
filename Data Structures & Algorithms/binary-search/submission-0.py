81ebbf2373a3a851b510c9ed0aa62ffa7d98d54b
iliyaz7 <syedmohammadiliyaz7@gmail.com>
2026-07-05 21:52:59 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+      left=0
+      right=len(nums)-1
+      while left<=right:
+        mid=left+(right-left)//2
+        if nums[mid]==target:
+          return mid
+        elif nums[mid]>target:
+          right=mid-1
+        else:
+          left=mid+1
+      return -1
+nums = [-1,0,2,4,6,8]
+target = 4
+sol=Solution()
+print(sol.search(nums,target))
